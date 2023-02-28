1edf87598d5fad219860566115e391359613d428
lleevvoonn <90211276+lleevvoonn@users.noreply.github.com>
2023-02-28 18:58:44 +0400

    Update Madlibs.py

--- Madlibs.py
+++ Madlibs.py
@@ -11,18 +11,18 @@ def story_1():
     Noun2 = input  ("Enter a noun")
     Number2 = int (input  ("Enter a number"))
     Noun3 = input ("Enter a noun")
-    Part_of_the_body2 = input  ("Enter a Part of the body")
-    Verb = input  ("Enter a verb")
-    Noun4 = input  ("Enter a noun")
-    Adjective3 = input  ("Enter an adjective")
-    Silly_word = input  ("Enter a Silly word")
-    Noun = input  ("Enter a noun")
+    Part_of_the_body2 = input ("Enter a Part of the body")
+    Verb = input ("Enter a verb")
+    Noun4 = input ("Enter a noun")
+    Adjective3 = input ("Enter an adjective")
+    Silly_word = input ("Enter a Silly word")
+    Noun = input ("Enter a noun")
 
     story1 = f"It was about {Number} {Measure_of_time} ago when I arrived at the hospital in a {Mode_of_transportation}. The hospital is a/an {Adjective} place, there are a lot of {Adjective2} {Noun} here. There are nurses here who have {Color} {Part_of_the_body}. If someone wants to come into my room I told them that they have to {Verb} first. I have decorated my room with {Number2} {Noun2}. Today I talked to a doctor and they were wearing a {Noun3} on their {Part_of_the_body2}. I heard that all doctors {Verb} {Noun4} every day for breakfast. The most {Adjective3} thing about being in the hospital is the {Silly_word} {Noun} !"
     print(story1)
     
 def story_2():
-    Persons_name = input  ("Enter a persons name")
+    Persons_name = input ("Enter a persons name")
     Noun = input  ("Enter a noun")
     Feeling = input  ("Enter a feeling")
     Verb = input  ("Enter a verb")
@@ -33,7 +33,7 @@ def story_2():
     Verb3 = input  ("Enter a verb")
     Adverb = input  ("Enter a adverb")
     Number = int (input  ("Enter a feeling"))
-    Measure_of_time = int (input  ("Enter a Measure of time"))
+    Measure_of_time = int (input ("Enter a Measure of time"))
     Color2 = input  ("Enter a color")
     Animal2 = input  ("Enter a animal")
     Noun = input  ("Enter a noun")
@@ -68,10 +68,10 @@ def story_3():
     story3 = f"Dear {Persons_name}, I am writing to you from a {Adjective} castle in an enchanted forest. I found myself here one day after going for a ride on a {Color} {animal} in {place}. There are {Adjective2} {Magical_creature} and {Adjective3} {Magical_creature2} here! In the {Room_in_a_house} there is a pool full of {Noun}. I fall asleep each night on a {Noun2} of {Noun3} and dream of {Adjective4} {Noun4}. It feels as though I have lived here for {Number} {Measure_of_time}. I hope one day you can visit, although the only way to get here now is {Verb} on a {Adjective5} {Noun5}!!"
     print(story3)
 
-a = input("Do you want to play MadLibs? y/n")
-if a == "yes":
+a = input("Do you want to play MadLibs? yes/no")
+if a.lower() == "yes":
     print("Ok, let's get started")
-    b = input("Choose template,  ")
+    b = input("Choose the template, 1, 2, 3 or random")
     if b == "3":
         story_3()
     elif b == "1":
@@ -88,5 +88,15 @@ if a == "yes":
             story_1()
         elif b == 2:
             story_2()
+            
+elif a.lower() == "no":
+    print("Fine, we are ending the game")
+    
+else:
+    print("Invalid answer")
 
+            
+            
+            
+            
     
